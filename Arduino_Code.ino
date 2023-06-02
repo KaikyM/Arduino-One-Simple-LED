eea154bb3c54e2e3b0f78cca06bb6384de140714
Kaiky Maurício Corrêa <kaikymauriciocorrea@gmail.com>
2023-06-02 19:25:57 -0300

    Create Arduino_Code.ino

---
+++ Arduino_Code.ino
@@ -0,0 +1,10 @@
+// C++ code 
+void setup(){
+   pinMode(LED_BUILTIN, OUTPUT);
+}
+void loop(){
+   delay(1000);
+   digitalWrite(LED_BUILTIN,LOW);
+   delay(1000);
+   digitalWrite(LED_BUILTIN,HIGH);
+}
